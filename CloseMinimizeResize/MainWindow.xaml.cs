1519242746b047289d1e692486003709955f2520
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-06-27 14:06:37 +0600

    3.0

--- CloseMinimizeResize/MainWindow.xaml.cs
+++ CloseMinimizeResize/MainWindow.xaml.cs
@@ -28,34 +28,70 @@ namespace CloseMinimizeResize
             InitializeComponent();
             Loaded += Window_Loaded;
         }
+
+        bool AlreadyFaded;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             this.MouseDown += delegate { DragMove(); };
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            WindowStyle = WindowStyle.SingleBorderWindow;
-            WindowState = WindowState.Minimized;
+            AlreadyFaded = false;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
-        }
-
-        protected override void OnActivated(EventArgs e)
-        {
-            base.OnActivated(e);
-            if (WindowStyle != WindowStyle.None)
+            //WindowStyle = WindowStyle.SingleBorderWindow;
+            if (!AlreadyFaded)
             {
-                Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, (DispatcherOperationCallback)delegate (object unused)
-                {
-                    WindowStyle = WindowStyle.None;
-                    return null;
-                }
-                , null);
+                AlreadyFaded = true;
+                var anim = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(1));
+                anim.Completed += new EventHandler(anim_Completed);
+                BeginAnimation(OpacityProperty, anim);
             }
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //WindowStyle = WindowStyle.SingleBorderWindow;
+            //WindowState = WindowState.Minimized;
+            if (!AlreadyFaded)
+            {
+                AlreadyFaded = true;
+                var anim = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(1));
+                anim.FillBehavior = FillBehavior.Stop;
+                anim.Completed += new EventHandler(minimizeCompleted);
+                this.BeginAnimation(UIElement.OpacityProperty, anim);
+            }
+        }
+
+        void anim_Completed(object sender, EventArgs e)
+        {
+            Close();
+        }
+        void minimizeCompleted(object sender, EventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            Opacity = 1;
+            AlreadyFaded = false;
+        }
+        void minimizeReset(object sender, EventArgs e)
+        {
+            Opacity = 1;
+            AlreadyFaded = false;
+        }
+
+
+
+        //protected override void OnActivated(EventArgs e)
+        //{
+        //    base.OnActivated(e);
+        //    if (WindowStyle != WindowStyle.None)
+        //    {
+        //        Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, (DispatcherOperationCallback)delegate (object unused)
+        //        {
+        //            WindowStyle = WindowStyle.None;
+        //            return null;
+        //        }
+        //        , null);
+        //    }
+        //}
     }
 }
